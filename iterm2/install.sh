3145da1ed342eea9ba2aeba92befc36c240762a0
Chris Paul <chris@hellosign.com>
2016-06-20 16:28:03 -0700

    Update iterm bootstrap. Remove node support

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -4,12 +4,16 @@
 # Terminal & iTerm 2                                                          #
 ###############################################################################
 
+cd ~/.dotfiles/iterm2
+
 # Install iTerm2 Fonts
-cp *.otf /Library/Fonts/
+cp -v ./*.otf /Library/Fonts/
 
 # Install pretty iTerm colors
 # TODO: Iterate over all files in ../iterm/presets?
-open presets/Symfonic.itermcolors
+open ./presets/Symfonic.itermcolors
 
 # Import saved iterm2 settings
 defaults import com.googlecode.iterm2 com.googlecode.iterm2.plist
+
+cd -
