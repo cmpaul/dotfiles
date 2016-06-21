91b61d7a61113cf95f3dbcec49c296755e0cb6a0
Chris Paul <chris@hellosign.com>
2016-06-20 17:12:57 -0700

    Move zsh to bash

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,14 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Node Version Manager
 #
-# This installs the node version manager
+# This installs the node version manager (the script itself has checks
+# to prevent duplicate installation)
 
-# Check for nvm
-if nvm ! $(command -v nvm)
-then
-  echo "  Installing nvm"
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
-fi
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
 
 exit 0
