9700fa04d0c7163e9e45befc708b954ff7567ab9
Chris Paul <chris@hellosign.com>
2016-07-17 09:21:46 -0700

    Update ngrok, atom, iterm2 and add powerline-shell config.

---
+++ powerline-shell/install.sh
@@ -0,0 +1,7 @@
+echo "› installing powerline-shell"
+cd ~
+git clone https://github.com/milkbikis/powerline-shell
+powerline-shell/install.py
+ln -s ~/powerline-shell/powerline-shell.py ~/powerline-shell.py
+
+# TODO: Copy the config.py file to the new ~/powerline-shell directory
