dcd492a030cdb52d8ab0933b0baa80302b8cfbaa
Chris Paul <cmpaul@gmail.com>
2016-07-17 09:38:53 -0700

    Fix powerline installer

--- powerline-shell/install.sh
+++ powerline-shell/install.sh
@@ -1,7 +1,10 @@
 echo "› installing powerline-shell"
-cd ~
-git clone https://github.com/milkbikis/powerline-shell
-powerline-shell/install.py
-ln -s ~/powerline-shell/powerline-shell.py ~/powerline-shell.py
-
-# TODO: Copy the config.py file to the new ~/powerline-shell directory
+CUR_DIR=`pwd`
+PL_DIR=$HOME/powerline-shell
+git clone https://github.com/milkbikis/powerline-shell $PL_DIR
+cd $PL_DIR 
+./install.py
+ln -f -s $HOME/powerline-shell/powerline-shell.py $HOME/powerline-shell.py
+ln -f -s $HOME/.dotfiles/powerline-shell/config.py $HOME/powerline-shell/config.py
+cd $CUR_DIR
+exit 0
