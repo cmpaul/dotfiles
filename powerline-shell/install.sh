47a79a45261ccf503012be6039708baec39ee818
Chris Paul <cmpaul@gmail.com>
2016-07-17 09:42:48 -0700

    Run install after linking config

--- powerline-shell/install.sh
+++ powerline-shell/install.sh
@@ -3,8 +3,9 @@ CUR_DIR=`pwd`
 PL_DIR=$HOME/powerline-shell
 git clone https://github.com/milkbikis/powerline-shell $PL_DIR
 cd $PL_DIR 
-./install.py
+# ./install.py
 ln -f -s $HOME/powerline-shell/powerline-shell.py $HOME/powerline-shell.py
 ln -f -s $HOME/.dotfiles/powerline-shell/config.py $HOME/powerline-shell/config.py
+./install.py
 cd $CUR_DIR
 exit 0
